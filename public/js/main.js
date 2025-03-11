c20f0eafd45001395c1b7f232d50a9fdfc31309d
Dilara <dilaraozturk028@gmail.com>
2025-03-11 09:58:29 +0100

    premiere question

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+console.log("Nous allons jouer à un quizz sur le tableau des types de Pokémon :)")
+do{
+var question = prompt ("Quel type est super efficace contre le type Combat ?");
+} while (question === "vol")
+    
